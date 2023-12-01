5012b7cd2e18e21a056504e0ef40e7dd6fb0f344
Tonevanda <jmguellourenco@gmail.com>
2023-12-01 20:19:48 +0000

    Create instructions.hs

---
+++ src/instructions.hs
@@ -0,0 +1,37 @@
+module Instructions where 
+
+import DataStructures
+
+
+--- Receives the stack and returns the updated stack
+--add :: Stack -> Stack
+
+-- Receives the stack and returns the updated stack
+--mult :: Stack -> Stack
+
+-- Receives the stack and returns the updated stack
+--sub :: Stack -> Stack
+
+-- Receives the stack and returns the updated stack
+--eq :: Stack -> Stack
+
+-- Receives the stack and returns the updated stack
+--le :: Stack -> Stack
+
+-- Receives a StackValue and the stack and returns the updated stack
+--push :: StackValue -> Stack -> Stack
+
+-- Receives a Key, the stack and the state and returns the updated stack
+--fetch :: Key -> Stack -> State -> Stack
+
+-- Receives a Key, the stack and the state and returns the updated stack and state
+--store :: Key -> Stack -> State -> Stack -> State
+
+-- Receives 2 code flows and the stack and returns one of the code flows and the updated stack
+--branch :: Code -> Code -> Stack -> Code -> Stack
+
+-- Receives 2 code flows, the stack and the state and returns the remaining code flow, the updated stack and updated state
+--loop :: Code -> Code -> Stack -> State -> Code -> Stack -> State
+
+-- ??????
+--noop :: 
