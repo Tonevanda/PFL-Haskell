96c50b5945635a02077a0d7cb1f7cc5707fc27a6
Tonevanda <jmguellourenco@gmail.com>
2023-12-01 20:20:10 +0000

    Update data.hs

--- src/data.hs
+++ src/data.hs
@@ -3,6 +3,8 @@
 module DataStructures where
 
 import qualified Data.Map.Strict as HashMap
+import Data.List (intercalate, sort)
+import Data.Map (toList)
 
 -- This defines the Instructions data type and the type Code, composed of Instructions 
 data Inst =
@@ -16,6 +18,11 @@ type Code = [Inst]
 data StackValue = IntValue Integer | TT | FF deriving Show
 type Stack = [StackValue]
 
+stackValueToString :: StackValue -> String
+stackValueToString (IntValue i) = show i
+stackValueToString TT = "True"
+stackValueToString FF = "False"
+
 push :: StackValue -> Stack -> Stack
 push x stack = x : stack
 
@@ -28,12 +35,20 @@ createEmptyStack = []
 
 -- This defines the State data type (Storage) and functions to operate on it
 type Key = String
-type Value = Integer
+type Value = StackValue
 
 type State = HashMap.Map Key Value
+
+pairToString :: (String, StackValue) -> String
+pairToString (key, IntValue i) = key ++ "=" ++ show i
+pairToString (key, TT) = key ++ "=True"
+pairToString (key, FF) = key ++ "=False"
 
 createEmptyState :: State
 createEmptyState = HashMap.empty
 
 insertIntoState :: Key -> Value -> State -> State
 insertIntoState = HashMap.insert
+
+stack2Str :: Stack -> String
+stack2Str stack = intercalate "," (map stackValueToString (reverse stack))
