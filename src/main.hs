4ae61f5425447e5c0916fd70f1598fd5a5ea7bee
Tonevanda <up202108863@up.pt>
2023-12-07 15:35:44 +0000

    organised tests

--- src/main.hs
+++ src/main.hs
@@ -3,17 +3,11 @@ module Main where
 import Datastructures
 import Interpreter
 import Compiler
-import Tests
+--import Tests
+
 
 -- main will just run all the tests, the function that processes the Code, Stack and State is the run function
 main :: IO ()
 main = do
-    print $ testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
-    print $ testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
-    print $ testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
-    print $ testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
-    print $ testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
-    print $ testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
-    print $ testAssembler [Push (-20),Push (-21), Le] == ("True","")
-    print $ testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
-    print $ testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
+    --runAllAssemblerTests
+    --runAllParserTests
