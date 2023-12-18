7006303b01e9ac19e28f6ed346796fdee6b5f371
Tonevanda <up202108863@up.pt>
2023-12-18 23:10:53 +0000

    Small refactor

--- src/interpreter.hs
+++ src/interpreter.hs
@@ -6,25 +6,27 @@ import Data.Map.Strict as HashMap
 run :: (Code, Stack, State) -> (Code, Stack, State)
 run ([], stack, state) = ([], stack, state)
 
-run (Noop:remainingCode, stack, state) = run (remainingCode, stack, state)
+run (Push n:remainingCode, stack, state) = run (remainingCode, IntValue n:stack, state)
 
-run (Loop code1 code2:remainingCode, stack, state) = run (code1 ++ [Branch (code2 ++ [Loop code1 code2]) [Noop]], stack, state)
+run (Add:remainingCode, IntValue i1:IntValue i2:stack, state) = run (remainingCode, IntValue (i1 + i2):stack, state)
+run (Add:remainingCode, _:_:stack, state) = error "Runtime error: Invalid operation"
 
-run (Branch code1 code2:remainingCode, TT:remainingStack, state) = run (code1, remainingStack, state)
-run (Branch code1 code2:remainingCode, FF:remainingStack, state) = run (code2, remainingStack, state)
-run (Branch code1 code2:remainingCode, IntValue i:remainingStack, state) = error "Runtime error: Invalid operation"
+run (Mult:remainingCode, IntValue i1:IntValue i2:stack, state) = run (remainingCode, IntValue (i1 * i2):stack, state)
+run (Mult:remainingCode, _:_:stack, state) = error "Runtime error: Invalid operation"
 
-run (Store key:remainingCode, stacktop:stack, state) = run (remainingCode, stack, insertIntoState key stacktop state)
+run (Sub:remainingCode, IntValue i1:IntValue i2:stack, state) = run (remainingCode, IntValue (i1 - i2):stack, state)
+run (Sub:remainingCode, _:_:stack, state) = error "Runtime error: Invalid operation"
 
-run (Fetch key:remainingCode, stack, state) =
-    run (remainingCode, newStack, state) 
-    where newStack = case HashMap.lookup key state of
-                          Just value -> pushToStack value stack
-                          Nothing -> error "Runtime error: Invalid operation"
+run (Tru:remainingCode, stack, state) = run (remainingCode, TT:stack, state)
 
-run (Neg:remainingCode, TT:stack, state) = run (remainingCode, FF:stack, state)
-run (Neg:remainingCode, FF:stack, state) = run (remainingCode, TT:stack, state)
-run (Neg:remainingCode, IntValue i:stack, state) = error "Runtime error: Invalid operation"
+run (Fals:remainingCode, stack, state) = run (remainingCode, FF:stack, state)
+
+run (Equ:remainingCode, value1:value2:stack, state) = run (remainingCode, value:stack, state)
+    where value = if value1 == value2 then TT else FF
+
+run (Le:remainingCode, IntValue i1:IntValue i2:stack, state) = run (remainingCode, value:stack, state)
+    where value = if i1 <= i2 then TT else FF
+run (Le:remainingCode, _:_:stack, state) = error "Runtime error: Invalid operation"
 
 run (And:remainingCode, TT:TT:stack, state) = run (remainingCode, TT:stack, state)
 run (And:remainingCode, TT:FF:stack, state) = run (remainingCode, FF:stack, state)
@@ -32,20 +34,22 @@ run (And:remainingCode, FF:TT:stack, state) = run (remainingCode, FF:stack, stat
 run (And:remainingCode, FF:FF:stack, state) = run (remainingCode, FF:stack, state)
 run (And:remainingCode, IntValue i:stack, state) = error "Runtime error: Invalid operation"
 
-run (Le:remainingCode, IntValue i1:IntValue i2:stack, state) = run (remainingCode, value:stack, state)
-    where value = if i1 <= i2 then TT else FF
+run (Neg:remainingCode, TT:stack, state) = run (remainingCode, FF:stack, state)
+run (Neg:remainingCode, FF:stack, state) = run (remainingCode, TT:stack, state)
+run (Neg:remainingCode, IntValue i:stack, state) = error "Runtime error: Invalid operation"
 
-run (Equ:remainingCode, value1:value2:stack, state) = run (remainingCode, value:stack, state)
-    where value = if value1 == value2 then TT else FF
+run (Fetch key:remainingCode, stack, state) =
+    run (remainingCode, newStack, state) 
+    where newStack = case HashMap.lookup key state of
+                          Just value -> value:stack
+                          Nothing -> error "Runtime error: Invalid operation"
 
-run (Fals:remainingCode, stack, state) = run (remainingCode, FF:stack, state)
+run (Store key:remainingCode, stacktop:stack, state) = run (remainingCode, stack, insertIntoState key stacktop state)
 
-run (Tru:remainingCode, stack, state) = run (remainingCode, TT:stack, state)
+run (Branch code1 code2:remainingCode, TT:remainingStack, state) = run (code1, remainingStack, state)
+run (Branch code1 code2:remainingCode, FF:remainingStack, state) = run (code2, remainingStack, state)
+run (Branch code1 code2:remainingCode, IntValue i:remainingStack, state) = error "Runtime error: Invalid operation"
 
-run (Sub:remainingCode, IntValue i1:IntValue i2:stack, state) = run (remainingCode, IntValue (i1 - i2):stack, state)
+run (Loop code1 code2:remainingCode, stack, state) = run (code1 ++ [Branch (code2 ++ [Loop code1 code2]) [Noop]], stack, state)
 
-run (Mult:remainingCode, IntValue i1:IntValue i2:stack, state) = run (remainingCode, IntValue (i1 * i2):stack, state)
-
-run (Add:remainingCode, IntValue i1:IntValue i2:stack, state) = run (remainingCode, IntValue (i1 + i2):stack, state)
-
-run (Push n:remainingCode, stack, state) = run (remainingCode, IntValue n:stack, state)
+run (Noop:remainingCode, stack, state) = run (remainingCode, stack, state)
