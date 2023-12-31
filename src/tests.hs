4427b712761f9263385b1b467e2a22df45010645
Tonevanda <jmguellourenco@gmail.com>
2023-12-31 21:32:58 +0000

    added more tests

--- src/tests.hs
+++ src/tests.hs
@@ -10,20 +10,20 @@ testAssembler code = (stack2Str stack, state2Str state)
 
 runAllAssemblerTests :: IO ()
 runAllAssemblerTests = do
-    print "Test assembler:"
-    print $ testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
-    print $ testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
-    print $ testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
-    print $ testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
-    print $ testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
-    print $ testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
-    print $ testAssembler [Push (-20),Push (-21), Le] == ("True","")
-    print $ testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
-    print $ testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
-    
-    --These should throw run-time errors:
-    --print $ testAssembler [Push 1,Push 2,And]
-    --print $ testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
+  putStrLn "Assembler tests:"
+  putStr "1: " >> print (testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10",""))
+  putStr "2: " >> print (testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True"))
+  putStr "3: " >> print (testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False"))
+  putStr "4: " >> print (testAssembler [Push (-20),Tru,Fals] == ("False,True,-20",""))
+  putStr "5: " >> print (testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20",""))
+  putStr "6: " >> print (testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20",""))
+  putStr "7: " >> print (testAssembler [Push (-20),Push (-21), Le] == ("True",""))
+  putStr "8: " >> print (testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4"))
+  putStr "9: " >> print (testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1"))
+  
+  --These should throw run-time errors:
+  --putStr "10: " >> print (testAssembler [Push 1,Push 2,And])
+  --putStr "11: " >> print (testAssembler [Tru,Tru,Store "y", Fetch "x",Tru])
 
 testParser :: String -> (String, String)
 testParser programCode = (stack2Str stack, state2Str state)
@@ -32,7 +32,8 @@ testParser programCode = (stack2Str stack, state2Str state)
 
 runAllParserTests :: IO ()
 runAllParserTests = do
-  print "Test parser:"
+  putStrLn ""
+  print "Parser tests:"
   putStr "1: " >> print (testParser "x := 5; x := x - 1;" == ("","x=4"))
   putStr "2: " >> print (testParser "x := 0 - 2;" == ("","x=-2"))
   putStr "3: " >> print (testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2"))
@@ -50,9 +51,16 @@ runAllParserTests = do
   putStr "15: " >> print (testParser "x:=3-1*3+(2-2*1+1-1);" == ("","x=0"))
   putStr "16: " >> print (testParser "x_x:=3-1*3+(2-2*1+1-1);" == ("","x_x=0"))
   putStr "17: " >> print (testParser "if (True) then () else ();" == ("",""))
-  putStr "18: " >> print (testParser  "x := 1; y := 2; z := y+x;" == ("","x=1,y=2,z=3"))
+  putStr "18: " >> print (testParser "x := 1; y := 2; z := y+x;" == ("","x=1,y=2,z=3"))
   putStr "19: " >> print (testParser "a := 1071; b := 462; while (not(a == 0)) do (temp := a; while (a <= b) do (b := b - a;); a := b; b := temp;); gcd := b;" == ("","a=0,b=21,gcd=21,temp=21"))
   putStr "20: " >> print (testParser "x := 5; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;););" == ("","x=5,y=1"))
   putStr "21: " >> print (testParser "x := 10; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;););" == ("","x=10,y=2"))
   putStr "22: " >> print (testParser "x := 15; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;););" == ("","x=15,y=3"))
   putStr "23: " >> print (testParser "x := 20; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;););" == ("","x=20,y=4"))
+  putStr "24: " >> print (testParser "x := 5; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;);); z := y;" == ("","x=5,y=1,z=1"))
+  putStr "25: " >> print (testParser "x := 10; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;);); z := y;" == ("","x=10,y=2,z=2"))
+  putStr "26: " >> print (testParser "x := 15; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;);); z := y;" == ("","x=15,y=3,z=3"))
+  putStr "27: " >> print (testParser "x := 20; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;);); z := y;" == ("","x=20,y=4,z=4"))
+  putStr "28: " >> print (testParser "x := 5; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;);); z := y; if z <= 1 then w := 1; else (if z <= 2 then w := 2; else (if z <= 3 then w := 3; else w := 4;););" == ("","w=1,x=5,y=1,z=1"))
+  putStr "29: " >> print (testParser "x := 10; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;);); z := y; if z <= 1 then w := 1; else (if z <= 2 then w := 2; else (if z <= 3 then w := 3; else w := 4;););" == ("","w=2,x=10,y=2,z=2"))
+  putStr "30: " >> print (testParser "x := 15; if x <= 5 then y := 1; else (if x <= 10 then y := 2; else (if x <= 15 then y := 3; else y := 4;);); z := y; if z <= 1 then w := 1; else (if z <= 2 then w := 2; else (if z <= 3 then w := 3; else w := 4;););" == ("","w=3,x=15,y=3,z=3"))
