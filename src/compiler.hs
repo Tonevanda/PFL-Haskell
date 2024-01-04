3d326c1ce4c493d367e2834ac2d924c45586697f
Tonevanda <jmguellourenco@gmail.com>
2024-01-04 17:06:46 +0000

    fixed bug in if

--- src/compiler.hs
+++ src/compiler.hs
@@ -74,9 +74,10 @@ parseIf ("if":tokens) = case break (=="then") tokens of
             let ")":thenStm = reverse thenStm'
                 (elseStm,")":";":next) = breakOnParenthesis ([],elseStm') 0
             in If (parseBexp ifStm) (parseProgram (reverse thenStm)) (parseProgram elseStm):parseProgram next
-        ("then":"(":thenStm', "else":elseStm) ->
+        ("then":"(":thenStm', "else":elseStm') ->
             let ")":thenStm = reverse thenStm'
-            in [If (parseBexp ifStm) (parseProgram (reverse thenStm)) [parseAssign elseStm]]
+                (elseStm, ";":next) = break (==";") elseStm'
+            in (If (parseBexp ifStm) (parseProgram (reverse thenStm)) [parseAssign elseStm]):parseProgram next
         ("then":thenStm, "else":"(":elseStm') ->
             let (elseStm,")":";":next) = breakOnParenthesis ([],elseStm') 0
             in (If (parseBexp ifStm) [parseAssign thenStm] (parseProgram elseStm):parseProgram next)
